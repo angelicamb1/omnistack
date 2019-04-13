2e5bfe2070840c6486826275915c38a70352f2c1
angelicamb1 <kita.angelica@gmail.com>
2019-04-12 21:09:59 -0300

    novos ajustes

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
+const cors = require('cors'); 
 
 const app = express();
-
 app.use(cors());
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-io.on('connection', socket =>{
-    socket.on('connectRoom', box =>{
+
+io.on('connection', (socket) => {
+    socket.on('connectRoom', (box) => {
         socket.join(box);
-    })
+    });
 });
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-g9xax.mongodb.net/omnistack?retryWrites=true', 
-{
-    useNewUrlParser:true
-});
+mongoose.connect(
+    'mongodb+srv://omnistack:omnistack@cluster0-7ejld.mongodb.net/test?retryWrites=true',
+    {
+        useNewUrlParser: true,
+    },
+);
 
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
     req.io = io;
 
     return next();
@@ -29,8 +31,9 @@ app.use((req,res,next) =>{
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/files', express.static(path.resolve(__dirname, '..' , 'tmp')));
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
 app.use(require('./routes'));
 
-app.listen(process.env.PORT || 3333); 
+server.listen(process.env.PORT || 3333);
